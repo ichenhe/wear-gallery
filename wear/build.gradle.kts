4b336e94c954675fdbcf64f72eeb7f1181a35f51
Chenhe <liangchenhe55@gmail.com>
2021-08-23 14:55:56 +0800

    prepare testing for galaxy watch4

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "cc.chenhe.weargallery"
         minSdk = 21
         targetSdk = 30
-        versionCode = 220601021 // header(22)+xx.xx.xx+device(0-phone; 1-wear)
-        versionName = "v6.1.2-preview"
+        versionCode = 220601031 // header(22)+xx.xx.xx+device(0-phone; 1-wear)
+        versionName = "v6.1.3-preview"
 
         kapt {
             arguments {
